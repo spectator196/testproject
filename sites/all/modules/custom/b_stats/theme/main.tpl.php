c4c29125acf2ed2d7fbb08f9d772906f695048dd
perfection <shark196@inbox.ru>
2023-03-23 11:04:47 +0300

    Реализация вывода неполной статистики в tpl.php

--- sites/all/modules/custom/b_stats/theme/main.tpl.php
+++ sites/all/modules/custom/b_stats/theme/main.tpl.php
@@ -4,7 +4,6 @@
  */
 ?>
 
-
 <!-- Bootstrap CSS -->
 <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/css/bootstrap.min.css" integrity="sha384-xOolHFLEh07PJGoPkLv1IbcEPTNtaed2xpHsD9ESMhqIYd0nLMwNLD69Npy4HI+N" crossorigin="anonymous">
 
@@ -12,33 +11,22 @@
 <script src="https://cdn.jsdelivr.net/npm/jquery@3.5.1/dist/jquery.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-Fy6S3B9q64WdZWQUiU+q4/2Lc9npb8tCaSX9FK7E8HnRr0Jz8D6OP9dO5Vg3Q9ct" crossorigin="anonymous"></script>
 
-
 <table class="table">
   <thead>
     <tr>
       <th scope="col">ID</th>
-      <th scope="col">Имя</th>
-      <th scope="col">Фамилия</th>
-      <th scope="col">Обращение</th>
+      <th scope="col">ФИО</th>
+      <th scope="col">ИНН</th>
+      <th scope="col">Email</th>
+      <th scope="col">Телефон</th>
+      <th scope="col">Форма</th>
     </tr>
   </thead>
   <tbody>
-    <tr>
-      <th scope="row">0</th>
-      <td><?php print $data_list[0][0];?></td>
-      <td>Otto</td>
-      <td>@mdo</td>
-    </tr>
-    <tr>
-      <th scope="row">2</th>
-      <td>Jacob</td>
-      <td>Thornton</td>
-      <td>@fat</td>
-    </tr>
-    <tr>
-      <th scope="row">3</th>
-      <td colspan="2">Larry the Bird</td>
-      <td>@twitter</td>
-    </tr>
+    <?php for ($i = 0; $i<count($data_list); $i++ ){
+      $record= '<tr><th scope="row">'.$data_list[$i][0].'</th><td>'.$data_list[$i][2].' '.$data_list[$i][3].' '.$data_list[$i][4].
+      '</td><td>'.$data_list[$i][5].'</td><td>'.$data_list[$i][8].'</td><td>'.$data_list[$i][9].'</td><td>'.$data_list[$i][6].'</td></tr>';
+      print $record;
+    } ?>
   </tbody>
 </table>
