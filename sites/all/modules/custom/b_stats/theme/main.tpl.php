e0b603a978ad9696e93de316865b5929427a8ebe
perfection <shark196@inbox.ru>
2023-03-24 20:26:18 +0300

    Обновление в .module и tpl.php

--- sites/all/modules/custom/b_stats/theme/main.tpl.php
+++ sites/all/modules/custom/b_stats/theme/main.tpl.php
@@ -1,10 +1,9 @@
 <?php
 /**
- * @var $data_list
+ * @var $full_data_list
+ * @var $main_data_list
  */
 ?>
-
-
 
 <!-- Bootstrap CSS -->
 <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/css/bootstrap.min.css" integrity="sha384-xOolHFLEh07PJGoPkLv1IbcEPTNtaed2xpHsD9ESMhqIYd0nLMwNLD69Npy4HI+N" crossorigin="anonymous">
@@ -13,30 +12,29 @@
 <script src="https://cdn.jsdelivr.net/npm/jquery@3.5.1/dist/jquery.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-Fy6S3B9q64WdZWQUiU+q4/2Lc9npb8tCaSX9FK7E8HnRr0Jz8D6OP9dO5Vg3Q9ct" crossorigin="anonymous"></script>
 
+<?php foreach ($main_data_list as $record) : ?>
+  <div class="card w-100">
+    <div class="row">
+      <div class="col"><b><?php print $record['id']['val']?> <?php print $record['full_name']['val']?></b>
+        <p><?php print $record['inn']['col']?>: <?php print $record['inn']['val']?></p>
+        <p><?php print $record['email']['col']?>: <?php print $record['email']['val']?></p>
+        <p><?php print $record['number']['col']?>: <?php print $record['number']['val']?></p>
+        <p><?php print $record['reg_form']['col']?>: <?php print $record['reg_form']['val']?></p>
+      </div>
+      <div class="col"><b><?php print $record['id']['val']?> <?php print $record['full_name']['val']?></b>
+        <p><?php print $record['inn']['col']?>: <?php print $record['inn']['val']?></p>
+        <p><?php print $record['email']['col']?>: <?php print $record['email']['val']?></p>
+        <p><?php print $record['number']['col']?>: <?php print $record['number']['val']?></p>
+        <p><?php print $record['reg_form']['col']?>: <?php print $record['reg_form']['val']?></p>
+      </div>
+    </div>
+  </div>
+  <?php endforeach; ?>
 
 
-<table class="table">
-  <thead>
-    <tr>
-      <th scope="col">ID</th>
-      <th scope="col">ФИО</th>
-      <th scope="col">ИНН</th>
-      <th scope="col">Email</th>
-      <th scope="col">Телефон</th>
-      <th scope="col">Форма</th>
-    </tr>
-  </thead>
-  <tbody>
-    <?php 
-    foreach ($data_list as $record){
-      $row='<tr><th scope="row">'.$record['ID'].'</th>
-      <td>'.$record['Full_name'].'</td>
-      <td>'.$record['Inn'].'</td>
-      <td>'.$record['Email'].'</td>
-      <td>'.$record['Phonenumber'].'</td>
-      <td>'.$record['Reg_form'].'</td>
-      </tr>';
-      print $row;
-    }?>
-  </tbody>
-</table>
+
+
+
+
+
+
